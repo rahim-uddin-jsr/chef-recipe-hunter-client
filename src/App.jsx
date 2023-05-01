7171a14f857ab5c4ed6994799bdcebe1b1dd4308
dev-rahim <rahimuddin5915@gmail.com>
2023-05-01 23:09:29 +0600

    ïnitial project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">Hello there</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
