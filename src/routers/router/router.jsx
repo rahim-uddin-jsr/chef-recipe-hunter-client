29fb36c67b9aafcfb04bf606622d228a9b8ba302
dev-rahim <rahimuddin5915@gmail.com>
2023-05-02 11:13:36 +0600

    navigationbar added

--- src/routers/router/router.jsx
+++ src/routers/router/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main/Main";
+import Blog from "../../Pages/Blog/Blog";
 import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <Home /> },
+      { path: "/blog", element: <Blog /> },
       { path: "/login", element: <Login /> },
       { path: "/register", element: <Registration /> },
     ],
--- src/shared/NavigationBar/NavigationBar.jsx
+++ src/shared/NavigationBar/NavigationBar.jsx
@@ -1,9 +1,92 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
-    <div>
-      <h2>Nav</h2>
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <NavLink
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li tabIndex={0}>
+              <NavLink
+                to="/blog"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending justify-between"
+                    : isActive
+                    ? "active justify-between"
+                    : "justify-between"
+                }
+              >
+                Blog
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          Foodie Crush
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li tabIndex={0}>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+              }
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="avatar tooltip" data-tip="Hello">
+          <div className="w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
