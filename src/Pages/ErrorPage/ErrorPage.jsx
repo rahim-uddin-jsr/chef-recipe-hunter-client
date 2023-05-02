7d2992068c8d46ec64530e10e93805728e70a803
dev-rahim <rahimuddin5915@gmail.com>
2023-05-02 21:54:55 +0600

    added error pagedesign

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,30 @@
+import Lottie from "lottie-react";
 import React from "react";
+import { useNavigate, useRouteError } from "react-router-dom";
 
+import theRobotBrokeAnd404Error from "../../../public/119322-the-robot-broke-and-404-error.json";
 const ErrorPage = () => {
+  const navigate = useNavigate();
+  const handleBackHome = () => {
+    navigate("/");
+  };
+  const { error, status } = useRouteError();
   return (
-    <div>
-      <h2>Error page</h2>
+    <div className="  ">
+      <div className="w-48 md:w-96 mx-auto my-12">
+        <Lottie animationData={theRobotBrokeAnd404Error} loop={true}></Lottie>
+      </div>
+      <span className="text-5xl mt-5 font-bold text-accent">{status}</span>
+      <br />
+      <br />
+      <span className="text-2xl mt-5">{error.message}</span> <br />
+      <br />
+      <button
+        className="btn btn-accent btn-lg text-white"
+        onClick={handleBackHome}
+      >
+        Back to Home
+      </button>
     </div>
   );
 };
